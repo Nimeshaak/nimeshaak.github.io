e2820a449c30a5c10b75f57c51616024d9e49995
Nimesha Warnakulasooriya <95769124+Nimeshaak@users.noreply.github.com>
2024-07-05 22:43:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -120,31 +120,31 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     document.getElementById("element2").addEventListener("click", function() {
-        openOverlay("project2.html");
+        openOverlay("Project2.html");
     });
 
     document.getElementById("element3").addEventListener("click", function() {
-        openOverlay("project3.html");
+        openOverlay("Project3.html");
     });
 
     document.getElementById("element4").addEventListener("click", function() {
-        openOverlay("project4.html");
+        openOverlay("Project4.html");
     });
 
     document.getElementById("element5").addEventListener("click", function() {
-        openOverlay("project5.html");
+        openOverlay("Project5.html");
     });
 
     document.getElementById("element6").addEventListener("click", function() {
-        openOverlay("project6.html");
+        openOverlay("Project6.html");
     });
 
     document.getElementById("element7").addEventListener("click", function() {
-        openOverlay("project7.html");
+        openOverlay("Project7.html");
     });
 
     document.getElementById("element8").addEventListener("click", function() {
-        openOverlay("project8.html");
+        openOverlay("Project8.html");
     });
 
     document.getElementById("overlay").addEventListener("click", function(event) {
